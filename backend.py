491daa5532c7a5fb8f8e9e1bfe7b691a3c46ea45
Riandi Ramadhan <38242309+ramadhanriandi@users.noreply.github.com>
2019-02-12 01:51:50 +0700

    fix mantap

--- backend.py
+++ backend.py
@@ -1,33 +1,41 @@
-def difference(result) :
-     return abs(eval(result) - 24)
+def difference(r_temp) :
+     return abs(r_temp - 24)
 
 def solving(numbers):
     op_counter = 3
-    operator = []
     point = 0
     numbers.sort(reverse = True)
-    result = str(numbers[0])
+    r_temp = int(numbers[0])
+    result = [numbers[0]]
     del numbers[0]
     
-    while (difference(result) != 0) and (op_counter != 0):
+    while (difference(r_temp) != 0) and (op_counter != 0):
         diff_temp = 0
         op_temp = ''
         for n in numbers:
-            diff_temp = difference(result + '+' + str(n))
+            diff_temp = difference(r_temp + int(n))
             op_temp = '+'
-            if difference(result + '*' + str(n)) < diff_temp:
-                diff_temp = difference(result + '*' + str(n))
+            if difference(r_temp * int(n)) < diff_temp:
+                diff_temp = difference(r_temp * int(n))
                 op_temp = '*'
-            if difference(result + '-' + str(n)) < diff_temp:
-                diff_temp = difference(result + '-' + str(n))
+            if difference(r_temp - int(n)) < diff_temp:
+                diff_temp = difference(r_temp - int(n))
                 op_temp = '-'
-            if difference(result + '/' + str(n)) < diff_temp:
-                diff_temp = difference(result + '/' + str(n))
+            if difference(r_temp / int(n)) < diff_temp:
+                diff_temp = difference(r_temp / int(n))
                 op_temp = '/'
-            result = result + op_temp + str(n)
-            operator.append(op_temp)
+            result.append(op_temp)
+            result.append(n)
+            r_temp = eval(''.join(result))
             op_counter -= 1
 
-    result = result + '=' + str(eval(result))
+    if (result[5] == '*' or result[5] == '/') and (result[3] == '+' or result[3] == '-'):
+        result.insert(0, '(')
+        result.insert(6, ')')
+    elif (result[3] == '*' or result[3] == '/') and (result[1] == '+' or result[1] == '-'):
+        result.insert(0, '(')
+        result.insert(4, ')')
 
-    return result
+    expression = ''.join(result) + '=' + str(r_temp)
+
+    return expression
