4b76dc56060ea446435fac902932979ea80f11b1
Riandi Ramadhan <38242309+ramadhanriandi@users.noreply.github.com>
2019-02-12 10:53:08 +0700

    Add files via upload

--- src/backend.py
+++ src/backend.py
@@ -4,32 +4,33 @@ def difference(r_temp) :
 def solving(numbers):
     op_counter = 3
     point = 0
+    numbers = list(map(int, numbers))
     numbers.sort(reverse = True)
-    r_temp = int(numbers[0])
-    result = [numbers[0]]
+    r_temp = numbers[0]
+    result = [str(numbers[0])]
     del numbers[0]
     
     while (difference(r_temp) != 0) and (op_counter != 0):
         diff_temp = 0
         op_temp = ''
         for n in numbers:
-            diff_temp = difference(r_temp + int(n))
+            diff_temp = difference(r_temp + n)
             op_temp = '+'
-            if difference(r_temp * int(n)) < diff_temp:
-                diff_temp = difference(r_temp * int(n))
+            if difference(r_temp * n) < diff_temp:
+                diff_temp = difference(r_temp * n)
                 op_temp = '*'
-            if difference(r_temp - int(n)) < diff_temp:
-                diff_temp = difference(r_temp - int(n))
+            if difference(r_temp - n) < diff_temp:
+                diff_temp = difference(r_temp - n)
                 op_temp = '-'
-            if difference(r_temp / int(n)) < diff_temp:
-                diff_temp = difference(r_temp / int(n))
+            if difference(r_temp / n) < diff_temp:
+                diff_temp = difference(r_temp / n)
                 op_temp = '/'
             result.append(op_temp)
-            result.append(n)
-            if (op_temp == '+'): r_temp += int(n)
-            elif (op_temp == '*'): r_temp *= int(n)
-            elif (op_temp == '-'): r_temp -= int(n)
-            elif (op_temp == '/'): r_temp /= int(n)
+            result.append(str(n))
+            if (op_temp == '+'): r_temp += n
+            elif (op_temp == '*'): r_temp *= n
+            elif (op_temp == '-'): r_temp -= n
+            elif (op_temp == '/'): r_temp /= n
             op_counter -= 1
 
     if (result[5] == '*' or result[5] == '/') and (result[3] == '+' or result[3] == '-'):
